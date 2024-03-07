511b214b773fb490357ed62736aa865f3fc958f6
Kat Tannehill <katetannehill@gmail.com>
2024-03-07 14:29:34 -0700

    Set up RouterProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import routes from './routes';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
