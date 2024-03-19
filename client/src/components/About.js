ddc2a9870993e6f499e70d7fc742bf67921467d7
Kat Tannehill <katetannehill@gmail.com>
2024-03-19 12:20:47 -0600

    About page & rough draft copy

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,22 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-        About
+        <h1>I'm Kat Tannehill</h1>
+        <p>
+            a full-stack software engineer who sees coding as an artform. Just call me the digital da Vinci - I blend technical mastery and creative vision to craft innovative web apps that seamlessly unite beauty and functionality.
+        </p>
+        <p>
+            With a background in graphic design and a stint wrangling the chaos of four-legged patients (and their two-legged owners), I've mastered the art of creating order from mayhem. My secret weapons? An insatiably curious mind, a keen eye for detail, and the ability to seamlessly merge creativity with technical precision.
+        </p>
+        <p>
+            I wield an arsenal of skills including React, Python, Flask, and the masterful application of snarky comments littered throughout my code. Because what fun is coding if you can't inject a little humor? Though my puns are terrible, I make no apologies. It's a character flaw, I'm self-aware.
+        </p>
+        <p>
+            When I'm not pushing pixels or taming convoluted code logic, you'll likely find me in the kitchen - a culinary playground where I experiment with daring flavor combinations (some more successful than others). I also maintain a ever-growing jungle of  indoor plants that somehow defy my lack of a green thumb to keep on thriving.
+        </p>
+        <p>
+            At the end of the day, I live by two core principles: crafting clean, robust code that delivers meaningful solutions, and doing it all with a dash of wit that keeps things interesting. So if you need a versatile developer who can breathe life into your web apps while cracking the occasional terrible joke, I'm your gal. Let's create something extraordinary...with a side of puns, of course.
+        </p>
     </div>
   )
 }
