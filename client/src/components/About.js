f7fd86c83eb85c9385324c3b272553b4fc9594d4
Kat Tannehill <katetannehill@gmail.com>
2024-03-21 19:48:05 -0600

    Change to Google font and tweak line height & font size

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -11,14 +11,16 @@ const SKILLS = ["React", "JavaScript", "Python", "SQL", "Flask"]
 const About = () => {
   return (
     <div id="container">
-        <div>
+        <div className='two-thirds padded'>
             <h1>I'm Kat Tannehill</h1>
-            <img src={headshot} alt="Hi, I'm Kat!" />
+            <img src={headshot} alt="Hi, I'm Kat!" id='main-img'/>
         </div>
-        <div className="dark-green-bg">
-            <p>
-                Brownie candy liquorice tart gummies danish soufflé. Brownie wafer gummi bears macaroon jelly-o pastry sesame snaps chupa chups wafer. Pie tiramisu bear claw topping cupcake cotton candy donut chupa chups. Fruitcake carrot cake candy marshmallow chocolate cake. Candy canes sugar plum dragée jelly-o cookie candy. Dragée gummies bonbon chocolate bar jelly macaroon cupcake danish. Donut dessert cheesecake sesame snaps oat cake caramels chupa chups danish cake. Brownie cake sesame snaps icing lollipop macaroon chupa chups oat cake candy canes. Oat cake jelly beans jelly beans jujubes lemon drops jelly beans. Bear claw gingerbread sweet roll sesame snaps oat cake candy canes pudding macaroon danish. Biscuit wafer topping icing icing pastry chocolate donut pastry. Gingerbread chocolate sweet roll pudding shortbread jelly beans.
-            </p>
+        <div className="dark-green-bg padded">
+            <div className='two-thirds'>
+                <p>
+                    Brownie candy liquorice tart gummies danish soufflé. Brownie wafer gummi bears macaroon jelly-o pastry sesame snaps chupa chups wafer. Pie tiramisu bear claw topping cupcake cotton candy donut chupa chups. Fruitcake carrot cake candy marshmallow chocolate cake. Candy canes sugar plum dragée jelly-o cookie candy. Dragée gummies bonbon chocolate bar jelly macaroon cupcake danish. Donut dessert cheesecake sesame snaps oat cake caramels chupa chups danish cake. Brownie cake sesame snaps icing lollipop macaroon chupa chups oat cake candy canes. Oat cake jelly beans jelly beans jujubes lemon drops jelly beans. Bear claw gingerbread sweet roll sesame snaps oat cake candy canes pudding macaroon danish. Biscuit wafer topping icing icing pastry chocolate donut pastry. Gingerbread chocolate sweet roll pudding shortbread jelly beans.
+                </p>
+            </div>
         </div>
     </div>
   )
