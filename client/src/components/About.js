391bc49ba910834726dd0a92a629ad1bd5d9f02c
Kat Tannehill <katetannehill@gmail.com>
2024-03-25 16:37:34 -0600

    Styling tweaks

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -24,7 +24,7 @@ const About = () => {
             </div>
         </div>
         <div className="dark-green-bg padded">
-            <p>
+            <p className='two-thirds'>
                 Brownie candy liquorice tart gummies danish soufflé. Brownie wafer gummi bears macaroon jelly-o pastry sesame snaps chupa chups wafer. Pie tiramisu bear claw topping cupcake cotton candy donut chupa chups. Fruitcake carrot cake candy marshmallow chocolate cake. Candy canes sugar plum dragée jelly-o cookie candy. Dragée gummies bonbon chocolate bar jelly macaroon cupcake danish. Donut dessert cheesecake sesame snaps oat cake caramels chupa chups danish cake. Brownie cake sesame snaps icing lollipop macaroon chupa chups oat cake candy canes. Oat cake jelly beans jelly beans jujubes lemon drops jelly beans. Bear claw gingerbread sweet roll sesame snaps oat cake candy canes pudding macaroon danish. Biscuit wafer topping icing icing pastry chocolate donut pastry. Gingerbread chocolate sweet roll pudding shortbread jelly beans.
             </p>
         </div>
