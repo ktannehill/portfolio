0b5135b4a9524e7b74bfc139c289573998139c23
Kat Tannehill <katetannehill@gmail.com>
2024-03-25 16:44:41 -0600

    Create Projects/ Project components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import About from '../components/About'
+import Projects from '../components/Projects'
 
 const Home = () => {
   return (
     <div id="container">
       <About />
+      <Projects />
     </div>
   )
 }
