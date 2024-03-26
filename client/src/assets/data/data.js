dfd85167765b261b2851982b6904a1d896caee6d
Kat Tannehill <katetannehill@gmail.com>
2024-03-25 18:45:33 -0600

    Replace cupcake ipsum with draft copy

---
+++ client/src/assets/data/data.js
@@ -0,0 +1,39 @@
+const PROJECTS = [
+    {
+        'id': 1,
+        'image': '../images/project-1.png',
+        'title': 'Community Events Calendar',
+        'description': '',
+        'tech': ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+        'id': 2,
+        'image': '../images/project-2.png',
+        'title': 'Puppy PlayDate',
+        'description': '',
+        'tech': ['HTML', 'CSS', 'JavaScript', 'React', 'MUI'],
+    },
+    {
+        'id': 3,
+        'image': '../images/project-3.png',
+        'title': 'The Zen of Jeopardy',
+        'description': '',
+        'tech': ['Python', 'SQL', 'Rich'],
+    },
+    {
+        'id': 4,
+        'image': '../images/project-4.png',
+        'title': 'CovenCraft',
+        'description': '',
+        'tech': ['Python', 'Flask-SQLAlchemy', 'CSS', 'React', 'JavaScript'],
+    },
+    {
+        'id': 5,
+        'image': '../images/project-5.png',
+        'title': 'Paws-ibilities Rescue',
+        'description': '',
+        'tech': ['Python', 'Flask-SQLAlchemy', 'Flask-Mail', 'Marshmallow'],
+    }
+]
+
+export default PROJECTS
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import PROJECTS from "../assets/data/data"
+import Project from './Project'
+
+const mapped_projects = PROJECTS.map(proj => <Project />)
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div>
+        Projects
+        {/* {mapped_projects} */}
+    </div>
   )
 }
 
