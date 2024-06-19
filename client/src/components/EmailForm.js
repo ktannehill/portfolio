d39b1db03ca48a18ebd01f8c4710ef7a691b04d7
Kat Tannehill <katetannehill@gmail.com>
2024-06-19 12:16:40 -0600

    Add message to form submit

--- client/src/components/EmailForm.js
+++ client/src/components/EmailForm.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser';
 import env from "react-dotenv";
 import { toast } from 'react-toastify';
@@ -7,39 +7,55 @@ const service_id = env?.REACT_APP_email_service_id
 const public_key = env?.REACT_APP_email_public_key
 
 const EmailForm = () => {
-    const form = useRef();
+    // const form = useRef();
+    const [formData, setFormData] = useState({
+        from_name: '',
+        from_email: '',
+        message: ''
+      });
+
+      console.log(formData)
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value
+        });
+      };
 
     const sendEmail = (e) => {
         e.preventDefault();
     
         emailjs
-            .sendForm(service_id, 'contact_form', form.current, {
+            .sendForm(service_id, 'contact_form', e.target, {
                 publicKey: public_key,
             })
             .then(
                 () => {
                     toast.success('Message sent successfully!');
-                    setFormData({ name: '', email: '', message: '' });
+                    setFormData({ from_name: '', from_email: '', message: '' });
                 },
                 (error) => {
                     console.log('FAILED...', error.text);
+                    toast.error('Failed to send message. Please try again later.')
                 },
             );
         e.target.reset()
     };
   
     return (
-        <form ref={form} onSubmit={sendEmail} id="form-container">
+        <form onSubmit={sendEmail} id="form-container">
             <label htmlFor='name'>Name
-                <input type="text" name="from_name" required />
+                <input type="text" name="from_name" value={formData.from_name} onChange={handleChange} required />
             </label>
             <label htmlFor='email'>Email
-                <input type="email" name="from_email" required/>
+                <input type="email" name="from_email" value={formData.from_email} onChange={handleChange} required/>
             </label>
             <label htmlFor='message'>Message
-                <textarea name="message" required/>
+                <textarea name="message" value={formData.message} onChange={handleChange} required/>
             </label>
-            <button type="submit" className='link-effect'>Submit</button>
+            <button type="submit" className='link-effect'>Send</button>
         </form>
     );
 }
