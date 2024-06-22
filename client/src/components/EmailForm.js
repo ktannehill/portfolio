14e1dcb38a6470a16506ec9485b6ee6123318785
Kat Tannehill <katetannehill@gmail.com>
2024-06-22 15:03:32 -0600

    Troubleshooting EmailJS undefined error upon send message

--- client/src/components/EmailForm.js
+++ client/src/components/EmailForm.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import emailjs from '@emailjs/browser';
-// import env from "react-dotenv";
+import env from "react-dotenv";
 import { toast } from 'react-toastify';
 
 const service_id = process.env?.REACT_APP_email_service_id
 const public_key = process.env?.REACT_APP_email_public_key
+
+console.log(service_id)
+console.log(public_key)
 
 const EmailForm = () => {
     const [formData, setFormData] = useState({
