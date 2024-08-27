a92dfcc1e04412530c1b383c92d8e52c7cb3a33b
Kat Tannehill <katetannehill@gmail.com>
2024-08-27 17:56:52 -0600

    Apply style to Contact section

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../style/contact.css'
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaMedium } from "react-icons/fa6";
