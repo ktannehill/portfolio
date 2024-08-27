e2e00af344e7a6e967f97eab9a0b5da2bcecab36
Kat Tannehill <katetannehill@gmail.com>
2024-08-27 16:24:52 -0600

    Commence breaking things - style refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
+import './style/global.css';
 import ScrollToAnchor from "./components/ScrollToAnchor";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  // const notify = () => toast("Wow so easy !");
 
   return (
     <div id="flex">
